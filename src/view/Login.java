fc31286752be2266bb17be4718ac6c2373d1c628
AlexAlvCe <49877987+AlexAlvCe@users.noreply.github.com>
2019-11-17 12:33:59 -0600

    Update Login interfaz

--- src/view/Login.java
+++ src/view/Login.java
@@ -1,16 +1,19 @@
 package view;
 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Login extends javax.swing.JFrame {
 	// Variables declaration - do not modify 
-    private javax.swing.JPanel panel;   
-    private javax.swing.JTextField email;
-    private javax.swing.JTextField pasword;
-    private javax.swing.JLabel labelEmail;
-    private javax.swing.JLabel labelLogin;
-    private javax.swing.JLabel labelPasword;
-    private javax.swing.JButton loginButton; 
-    private javax.swing.JButton registerButton;
+    private JPanel panel;   
+    private JTextField email;
+    private JTextField pasword;
+    private JLabel labelEmail;
+    private JLabel labelLogin;
+    private JLabel labelPasword;
+    private JButton loginButton; 
+    private JButton registerButton;
     // End of variables declaration 
     
 
@@ -22,126 +25,129 @@ public class Login extends javax.swing.JFrame {
     }
                                  
     private void initComponents() {
+    	this.setTitle("Login");
+        panel = new JPanel();
+        email = new JTextField();
+        pasword = new JTextField();
+        labelLogin = new JLabel();
+        labelPasword = new JLabel();
+        labelEmail = new JLabel();
+        loginButton = new JButton();
+        registerButton = new JButton();
 
-        panel = new javax.swing.JPanel();
-        email = new javax.swing.JTextField();
-        pasword = new javax.swing.JTextField();
-        labelLogin = new javax.swing.JLabel();
-        labelPasword = new javax.swing.JLabel();
-        labelEmail = new javax.swing.JLabel();
-        loginButton = new javax.swing.JButton();
-        registerButton = new javax.swing.JButton();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        panel.setBackground(new Color(102, 102, 102));       
 
-        panel.setBackground(new java.awt.Color(51, 0, 153));       
-
-        labelLogin.setFont(new java.awt.Font("Times New Roman", 0, 36)); 
-        labelLogin.setForeground(new java.awt.Color(255, 0, 0));
+        labelLogin.setFont(new Font("Times New Roman", 0, 36)); 
+        labelLogin.setForeground(new Color(0, 0, 102));
         labelLogin.setText("Login");
 
-        labelPasword.setFont(new java.awt.Font("Times New Roman", 0, 24)); 
-        labelPasword.setForeground(new java.awt.Color(255, 255, 255));
-        labelPasword.setText("Contraseña:");
+        labelPasword.setFont(new Font("Times New Roman", 0, 24)); 
+        labelPasword.setForeground(new Color(0, 0, 102));
+        labelPasword.setText("Pasword:");
 
-        labelEmail.setFont(new java.awt.Font("Times New Roman", 0, 24)); 
-        labelEmail.setForeground(new java.awt.Color(255, 255, 255));
-        labelEmail.setText("Correo:");
+        labelEmail.setFont(new Font("Times New Roman", 0, 24)); 
+        labelEmail.setForeground(new Color(0, 0, 102));
+        labelEmail.setText("Mail:");
 
-        loginButton.setBackground(new java.awt.Color(255, 0, 0));
-        loginButton.setFont(new java.awt.Font("Times New Roman", 0, 24)); 
-        loginButton.setText("Iniciar Sesión");
-        loginButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        loginButton.setBackground(new Color(0, 0, 102));
+        loginButton.setForeground(new Color(255, 255, 255));
+        loginButton.setFont(new Font("Times New Roman", 0, 24)); 
+        loginButton.setText("Log in: ");
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 loginButtonActionPerformed(evt);
             }
         });
 
-        registerButton.setBackground(new java.awt.Color(255, 0, 0));
-        registerButton.setFont(new java.awt.Font("Times New Roman", 0, 24)); 
-        registerButton.setText("Registrarse");
-        registerButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        registerButton.setBackground(new Color(0, 0, 102));
+        registerButton.setForeground(new Color(255, 255, 255));
+        registerButton.setFont(new Font("Times New Roman", 0, 24)); 
+        registerButton.setText("sign in: ");
+        registerButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 registerButtonActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
+        GroupLayout panelLayout = new GroupLayout(panel);
         panel.setLayout(panelLayout);
         panelLayout.setHorizontalGroup(
-            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            panelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(panelLayout.createSequentialGroup()
-                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(panelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                     .addGroup(panelLayout.createSequentialGroup()
-                        .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(email, javax.swing.GroupLayout.PREFERRED_SIZE, 267, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(panelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                            .addComponent(email, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
+                            .addGroup(panelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                 .addGroup(panelLayout.createSequentialGroup()
                                     .addGap(58, 58, 58)
-                                    .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(labelEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(labelPasword, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addGroup(panelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addComponent(labelEmail, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(labelPasword, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)))
                                 .addGroup(panelLayout.createSequentialGroup()
                                     .addGap(111, 111, 111)
-                                    .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(panelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                         .addComponent(loginButton)
-                                        .addComponent(pasword, javax.swing.GroupLayout.PREFERRED_SIZE, 267, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(pasword, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)))))
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(registerButton))
                     .addGroup(panelLayout.createSequentialGroup()
                         .addGap(208, 208, 208)
-                        .addComponent(labelLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(labelLogin, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(83, Short.MAX_VALUE))
         );
         panelLayout.setVerticalGroup(
-            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            panelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(panelLayout.createSequentialGroup()
-                .addComponent(labelLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(labelEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(email, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(labelLogin, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(labelEmail, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(email, GroupLayout.PREFERRED_SIZE, 59, GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(labelPasword, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(labelPasword, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(panelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                     .addGroup(panelLayout.createSequentialGroup()
-                        .addComponent(pasword, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(pasword, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(panelLayout.createSequentialGroup()
                         .addGap(0, 128, Short.MAX_VALUE)
-                        .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addGroup(panelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                             .addComponent(loginButton)
                             .addComponent(registerButton))
                         .addGap(57, 57, 57))))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        javax.swing.GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(panel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(panel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }                      
 
     
-    private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {                                            
+    private void loginButtonActionPerformed(ActionEvent evt) {                                            
         // TODO add your handling code here:
     	String mail = email.getText(); 
     	String pasw = pasword.getText(); 
     	
     }                                           
 
-    private void registerButtonActionPerformed(java.awt.event.ActionEvent evt) {                                               
+    private void registerButtonActionPerformed(ActionEvent evt) {                                               
         // TODO add your handling code here:
     	String mail = email.getText(); 
     	String pasw = pasword.getText(); 
     }                                              
 
 }
+
